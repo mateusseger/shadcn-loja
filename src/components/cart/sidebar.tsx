6883017697df180ed82653d3e618dc392957b1b5
Mateus <mateusseger@outlook.com>
2024-03-11 16:06:07 -0300

    finished project

--- src/components/cart/sidebar.tsx
+++ src/components/cart/sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Sheet,
   SheetContent,
@@ -8,30 +10,57 @@ import {
 import { Button } from "@/components/ui/button";
 import { RocketIcon } from "@radix-ui/react-icons";
 import { Separator } from "@/components/ui/separator";
+import { useCartStore } from "@/stores/cart-store";
+import { CartItem } from "@/components/cart/item";
+import { useState } from "react";
+import { CheckoutDialog } from "@/components/checkout/dialog";
 
 export const CartSidebar = () => {
+  const [checkoutOpen, setCheckoutOpen] = useState(false);
+
+  const { cart } = useCartStore((state) => state);
+  let subtotal = 0;
+
+  for (let item of cart) {
+    subtotal += item.quantity * item.product.price;
+  }
+
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button>
+        <Button className="relative">
           <RocketIcon className="mr-2" />
           <p>Carrinho</p>
+          {cart.length > 0 && (
+            <div className="absolute size-3 bg-red-600 rounded-full -right-1 -top-1"></div>
+          )}
         </Button>
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
           <SheetTitle>Carrinho</SheetTitle>
         </SheetHeader>
-        <div className="flex flex-col gap-5 my-3">...</div>
+        <div className="flex flex-col gap-5 my-3">
+          {cart.map((item) => (
+            <CartItem key={item.product.id} item={item} />
+          ))}
+        </div>
         <Separator className="my-4" />
         <div className="flex justify-between items-center text-xs">
           <div>Subtotal</div>
-          <div>...</div>
+          <div>R$ {subtotal.toFixed(2)}</div>
         </div>
         <Separator className="my-4" />
         <div className="text-center">
-          <Button>Finalizar Compra</Button>
+          <Button
+            onClick={() => setCheckoutOpen(true)}
+            disabled={cart.length === 0}
+          >
+            Finalizar Compra
+          </Button>
         </div>
+
+        <CheckoutDialog open={checkoutOpen} onOpenChange={setCheckoutOpen} />
       </SheetContent>
     </Sheet>
   );
---
+++ src/components/checkout/dialog.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Progress } from "@/components/ui/progress";
+import { useState } from "react";
+import { StepUser } from "@/components/checkout/user";
+import { StepAddress } from "@/components/checkout/address";
+import { StepFinish } from "@/components/checkout/finish";
+import { CheckoutSteps } from "@/types/checkout-steps";
+
+type Props = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+};
+
+export const CheckoutDialog = ({ open, onOpenChange }: Props) => {
+  const [step, setStep] = useState<CheckoutSteps>("user");
+
+  let progressPct = 0;
+  switch (step) {
+    case "user":
+      progressPct = 33;
+      break;
+    case "address":
+      progressPct = 66;
+      break;
+    case "finish":
+      progressPct = 100;
+      break;
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {step === "user" && "Dados Pessoais"}
+            {step === "address" && "Endereço de entrega"}
+            {step === "finish" && "Envio para o WhatsApp"}
+          </DialogTitle>
+        </DialogHeader>
+
+        <Progress value={progressPct} />
+        <div className="flex flex-col gap-3">
+          {step === "user" && <StepUser setStep={setStep} />}
+          {step === "address" && <StepAddress setStep={setStep} />}
+          {step === "finish" && <StepFinish />}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
