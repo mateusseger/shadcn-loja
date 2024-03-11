bb5827c6cd60d105b26e23d7fa42e7723e91a07b
Mateus <mateusseger@outlook.com>
2024-03-10 23:11:55 -0300

    first commit

---
+++ src/components/cart/sidebar.tsx
@@ -0,0 +1,38 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { RocketIcon } from "@radix-ui/react-icons";
+import { Separator } from "@/components/ui/separator";
+
+export const CartSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button>
+          <RocketIcon className="mr-2" />
+          <p>Carrinho</p>
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Carrinho</SheetTitle>
+        </SheetHeader>
+        <div className="flex flex-col gap-5 my-3">...</div>
+        <Separator className="my-4" />
+        <div className="flex justify-between items-center text-xs">
+          <div>Subtotal</div>
+          <div>...</div>
+        </div>
+        <Separator className="my-4" />
+        <div className="text-center">
+          <Button>Finalizar Compra</Button>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
